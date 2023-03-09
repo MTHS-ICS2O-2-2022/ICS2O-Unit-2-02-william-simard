a751532903d0e6533dbfee7a4add3198b38ab100
william <124389603+william-simard@users.noreply.github.com>
2023-03-09 17:51:56 +0000

    changed index and jave script

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+  onclick="doMathClicked()">
+  Do math
+</button> 
